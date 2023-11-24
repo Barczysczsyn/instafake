e55716b57b30226928bd77b9f3cb20368feaf00d
Joao Marcos Barczysczyn Vilhanueva <u05366703158@acad.pp.ifms.edu.br>
2023-11-24 12:44:34 -0300

    aula dia 24

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -11,13 +11,57 @@
           <ion-title size="large">Buscar</ion-title>
         </ion-toolbar>
       </ion-header>
+      <div id="camera"></div>
+      <div class="btns">
+        <IonButton @click="tirarFoto()">
+          Tirar Foto
+        </IonButton>
+      </div>
+      <IonGrid>
+        <IonRow>
+          <IonCol size="4" v-for="foto in fotos">
+            <IonImg :src="foto"></IonImg>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
 
+      
       <ExploreContainer name="Buscar page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonImg} from '@ionic/vue';
+import {CameraPreview} from "@capacitor-community/camera-preview";
+
+export default{
+name: "Criar",
+data(){
+  return{
+    fotos:[] as string[]
+  }
+},
+  components:{
+    IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonImg
+  },
+methods:{
+  abrirCamera(){
+    CameraPreview.start({
+      parent:"camera",
+      toBack: true,
+      position: "front"
+
+    })
+  },
+  async tirarFoto(){
+    const foto = await CameraPreview.captureSample({quality: 50})
+    const base64 = "data:image/jpeg;base64,"
+    this.fotos = [base64 + foto.value, ...this.fotos]
+  }
+},
+mounted(){
+  this.abrirCamera();
+}
+}
 </script>
--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -11,13 +11,67 @@
           <ion-title size="large">Perfil</ion-title>
         </ion-toolbar>
       </ion-header>
+      <!--<p v-for="post in listPosts">{{post.author  }}</p>-->
+      <ion-grid>
+        <ion-row>
+          <ion-col v-for="post in listPosts" size="4">
+              <img :src="infoPerfil.avatar_url" :alt="infoPerfil.login">
+          </ion-col>
+          <ion-col>
+            <p>{{ infoPerfil.public_repos }}</p>
+            <p>publicacoes</p>
+          </ion-col>
+          <ion-col>
+            <p>{{ infoPerfil.followers }}</p>
+            <p>seguidores</p>
+          </ion-col>
+          <ion-col>
+            <p>{{ infoPerfil.following }}</p>
+            <p>seguindo</p>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <p>{{ infoPerfil.name }}</p>
+      <p>@{{ infoPerfil.login }}</p>
+      <p>{{ infoPerfil.bio }}</p>
+      <ion-grid>
+        <ion-row>
+          <ion-col v-for="post in listPosts" size="4">
+            <a :href="post.url">
+              <img :src="post.download_url" :alt="post.author">
+            </a>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-      <ExploreContainer name="Perfil page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script  lang="ts">
+import { IonGrid, IonRow, IonCol, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import MyPost from '@/classes/MyPosts'
+export default{
+  name: "Perfil",
+  data(){
+    return{
+      infoPerfil: {},
+      listPosts: [] as MyPost[]
+    }
+  },
+  methods: {
+    async getFotos(){
+const fotos = await fetch("https://picsum.photos/v2/list?page=25");
+this.listPosts = await fotos.json();
+    },
+    async getInfo(){
+const infos = await fetch("https://api.github.com/users/carloscacho");
+this.listPosts = await infos.json();
+    }
+  },
+  mounted(){
+    this.getFotos();
+    this.getInfo();
+  }
+}
 </script>
